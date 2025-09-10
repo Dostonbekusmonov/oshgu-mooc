54fee10d9f7fd9cc259191c6409cbc3856df79b5
Dostonbekusmonov <157524329+Dostonbekusmonov@users.noreply.github.com>
2025-09-10 23:27:48 +0500

    Update script.js

--- backend/frontend/frontend/script.js
+++ backend/frontend/frontend/script.js
@@ -1,1 +1,10 @@
+document.getElementById("helloBtn").addEventListener("click", async () => {
+  try {
+    const response = await fetch("/api/hello");
+    const data = await response.json();
+    document.getElementById("response").innerText = data.message;
+  } catch (err) {
+    document.getElementById("response").innerText = "Serverga ulanishda muammo!";
+  }
+});
 
